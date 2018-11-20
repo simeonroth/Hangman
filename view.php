419181c23ca6a5e345081f414341321e79a4605b
Simeon Roth <simeonroth@email.arizona.edu>
2018-11-20 14:19:15 -0700

    viewUpdate

--- view.php
+++ view.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,32 +7,60 @@
 <body>
 <h3>Hangman</h3><br>
 
-<input id="input"></input>
+<input id="input" size="1" maxlength="1"></input>
 <button onclick="guessLetter()">Guess</button>
 <div id="div"></div>
+<div id="messages"></div>
 
 <script type="text/javascript">
+var attempts=0;
 var word = "hangman";
+var messages = document.getElementById("messages");
 var div = document.getElementById("div");
 var input = document.getElementById("input");
 var guess;
 var hiddenWord;
 var i;
-initWord()
+var correctGuess = 0;
+initWord();
 function guessLetter(){
 	guess =  input.value;
 	for (i=0;i<word.length;i++){
 		if (guess == word[i]){
 			hiddenWord[i]=guess;
+			correctGuess=1;
 		}
 	}
+	if (!correctGuess){
+		attempts ++;
+		messages.innerHTML="youve made "+attempts+" incorrect guesses";
+		
+		if (attempts>4){
+			messages.innerHTML="you Lose";
+			input.disabled=true;
+		}
+	} else {
+		if (ifOver()){
+			messages.innerHTML="you Win";
+			input.disabled=true;
+		}
+	}
+	correctGuess=0;
 	update();
+}
+
+function ifOver(){
+	for (i=0;i<word.length;i++){
+		if (hiddenWord[i]!=word[i])
+			return false;
+	}
+	return true;
 }
 
 function update(){
 	div.innerHTML="";
 	for (i=0;i<word.length;i++){
-		div.innerHTML+=hiddenWord[i];
+		div.innerHTML+=hiddenWord[i]+" ";
 	}
 	
 }
@@ -42,7 +70,7 @@ function initWord(){
 	for (i=0;i<word.length;i++){
 		hiddenWord[i]="_";
 	}
-
+	update();
 }
 
 
