c52d8fddb1dd6f5f5eb8f80eb75d99be4ecebda9
bma2 <bma2@email.arizona.edu>
2018-11-25 14:47:42 -0700

    fixed headers and echo

--- controller.php
+++ controller.php
@@ -2,17 +2,25 @@
 session_start();
 require_once "DBWordsAdapter.php";
 
-if(! isset($_GET['oneGame'])) {
-    $_GET['oneGame'] = new DBWordsAdapter();
-    $arr = $_GET['oneGame']->getRandomWord();
-    echo json_encode($arr);
-}
-
 if (isset($_GET['new'])) {
-    header ( "Location: view.php" ); 
+    header("Location: view.php");
 }
 if (isset($_GET['returning'])) {
-    header ( "Location: view.php" );
+    header("Location: view.php");
+}
+
+if(! isset($_GET['oneGame'])) {
+    $wordArray = array();
+    $parseLine = "";
+    $inputFile = fopen("words.txt", "r");
+    
+    while ($line = fgets($inputFile)) {
+        $parseLine = trim($line, "\r\n");
+        array_push($wordArray, $parseLine);
+    }
+    
+    $_GET['oneGame'] = $wordArray;
+    echo json_encode($_GET['oneGame']);
 }
 
 
