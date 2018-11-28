9b29673614cdf3c23538ff62eba2009fa7b359b3
bma2 <bma2@email.arizona.edu>
2018-11-28 15:28:28 -0700

    fixing

--- view.php
+++ view.php
@@ -21,8 +21,8 @@ session_start (); // Need this in each file before $_SESSION is used.
 
 <div id = "random"></div> <!-- So you can see what word is picked at random -->
 
-<canvas id="myCanvas" width="300" height="300">
-Your browser does not support the HTML5 canvas tag.</canvas><br><br>
+<!--<canvas id="myCanvas" width="300" height="300">
+Your browser does not support the HTML5 canvas tag.</canvas><br><br> -->
 
 <!-- <div id="div"></div><br> -->
 <!--  <input type="text" id="input" size="1" maxlength="1" ></input>
